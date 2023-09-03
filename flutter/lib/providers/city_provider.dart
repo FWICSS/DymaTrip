b0ad9c1df90874e2d17fef7cd73d0d1d2aca7faf
dim-sDev <dimitriaigle@gmail.com>
2023-09-03 00:39:53 -0400

    add Project

---
+++ flutter/lib/providers/city_provider.dart
@@ -0,0 +1,35 @@
+import 'dart:collection';
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/city_model.dart';
+
+class CityProvider extends ChangeNotifier {
+  final String host = 'localhost';
+  List<City> _cities = [];
+  bool isLoading = false;
+
+  UnmodifiableListView<City> get cities => UnmodifiableListView(_cities);
+
+  City getCityByName(String cityName) =>
+      cities.firstWhere((city) => city.name == cityName);
+
+  Future<void> fetchData() async {
+    try {
+      isLoading = true;
+      http.Response response = await http.get(Uri.http(host, '/api/cities'));
+      if (response.statusCode == 200) {
+        _cities = (json.decode(response.body) as List)
+            .map((cityJson) => City.fromJson(cityJson))
+            .toList();
+        isLoading = false;
+        notifyListeners();
+      }
+    } catch (e) {
+      isLoading = false;
+      rethrow;
+    }
+  }
+}
---
+++ flutter/lib/views/trip/trip_view.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../models/city_model.dart';
+import '../../providers/city_provider.dart';
+import 'widgets/trip_activities.dart';
+import 'widgets/trip_city_bar.dart';
+import 'widgets/trip_weather.dart';
+
+class TripView extends StatelessWidget {
+  static const String routeName = '/trip';
+
+  const TripView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final String cityName = (ModalRoute.of(context)!.settings.arguments
+        as Map<String, String>)['cityName']!;
+    final String tripId = (ModalRoute.of(context)!.settings.arguments
+        as Map<String, String>)['tripId']!;
+    final City city = Provider.of<CityProvider>(context, listen: false)
+        .getCityByName(cityName);
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            TripCityBar(
+              city: city,
+            ),
+            TripWeather(cityName: cityName),
+            TripActivities(tripId: tripId)
+          ],
+        ),
+      ),
+    );
+  }
+}
